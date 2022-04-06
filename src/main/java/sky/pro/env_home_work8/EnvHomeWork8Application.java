09cbfd21d4f19be4b610a19dd101d97a3e6b84c8
katya <kotenokches@rambler.ru>
2022-04-06 08:48:04 +0500

    Первый коммит

---
+++ src/main/java/sky/pro/env_home_work8/EnvHomeWork8Application.java
@@ -0,0 +1,13 @@
+package sky.pro.env_home_work8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvHomeWork8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvHomeWork8Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/env_home_work8/EnvHomeWork8ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.env_home_work8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvHomeWork8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
