de8bb4776d7592ef5d4051e74c92d2127950562d
katya <kotenokches@rambler.ru>
2022-04-30 08:06:39 +0500

    Домашка выполнена 15 -исправлена

--- src/test/java/sky/pro/env_home_work8/DepartmentServiceImplTest.java
+++ src/test/java/sky/pro/env_home_work8/DepartmentServiceImplTest.java
@@ -2,16 +2,22 @@ package sky.pro.env_home_work8;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sky.pro.env_home_work8.domain.Employee;
+import sky.pro.env_home_work8.exception.EmployeeNotFoundException;
 import sky.pro.env_home_work8.service.DepartmentServiceImpl;
 import sky.pro.env_home_work8.service.EmployeeService;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,33 +34,45 @@ public class DepartmentServiceImplTest {
     @InjectMocks
     private DepartmentServiceImpl out;
 
-    @Test
-    public void departmentTestMax() {
-        when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(54000, out.maxSalary(5));
-        when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(50000, out.maxSalary(5));
-        when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(50000, out.maxSalary(1));
-        when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(140000, out.maxSalary(1));
-        when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(140000, out.maxSalary(null));
+    private static Stream<Arguments> argumentsDepartmentTestMax() {
+        return Stream.of(
+                Arguments.of(5, 54000),
+                Arguments.of(1, 140000)
+        );
     }
 
-    @Test
-    public void departmentTestMin() {
+    @ParameterizedTest
+    @MethodSource("argumentsDepartmentTestMax")
+    public void paramTestMax(int department, int expected) {
         when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(50000, out.minSalary(0));
+        assertEquals(expected, out.maxSalary(department));
+    }
+
+    private static Stream<Arguments> argumentsDepartmentTestMin() {
+        return Stream.of(
+                Arguments.of(5, 50000),
+                Arguments.of(1, 100000)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsDepartmentTestMin")
+    public void paramTestMin(int department, int expected) {
         when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(50000, out.minSalary(5));
-        when(employeeService.findAll()).thenReturn(employees);
-        assertEquals(50000, out.minSalary(1));
+        assertEquals(expected, out.minSalary(department));
     }
 
     @Test
     public void departmentTestAmount() {
         when(employeeService.findAll()).thenReturn(employees);
         assertEquals("3120000,52000.0", out.amount(2));
+    }
+
+    @Test
+    public void testWithNullDepThrowsException() {
+        assertThrows(EmployeeNotFoundException.class, () -> out.maxSalary(null));
+        assertThrows(EmployeeNotFoundException.class, () -> out.minSalary(null));
+        assertThrows(EmployeeNotFoundException.class, () -> out.amount(null));
+
     }
 }
